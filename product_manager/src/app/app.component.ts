29181bfcdfc97cb0b361d6accd2e09dfc4e70160
vhcamposq <viniciushcquadros@hotmail.com>
2024-08-26 23:07:22 -0300

    Criando componente nav

--- product_manager/src/app/app.component.ts
+++ product_manager/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/template/header/header.component';
 import { FooterComponent } from './component/template/footer/footer.component';
+import { NavComponent } from './component/template/nav/nav.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
