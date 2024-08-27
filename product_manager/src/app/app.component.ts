46b3b3954934a7068359954dc3800524efdf0e34
vhcamposq <viniciushcquadros@hotmail.com>
2024-08-26 22:34:46 -0300

    Criando header e footer

--- product_manager/src/app/app.component.ts
+++ product_manager/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/template/header/header.component';
+import { FooterComponent } from './component/template/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
